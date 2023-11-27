f2f41120d529e017b334302d147e51f0a316f25d
3B032111 <3b032111@gm.student.ncut.edu.tw>
2023-11-27 20:44:29 +0800

    git set chipr route

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 Route::resource('chirps', ChirpController::class)
-    ->only(['index', 'store'])
+    ->only(['index', 'store', 'edit', 'update'])
     ->middleware(['auth', 'verified']);
 require __DIR__.'/auth.php';
